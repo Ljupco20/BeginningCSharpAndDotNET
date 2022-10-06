f87062c9ff6265251c6ac664197a5aa0fafef16e
Ljupco20 <ljupcotrajanovski@hotmail.com>
2022-10-06 18:15:55 +0200

    Started with Collections

---
+++ Chapter10/Ch10Exercises/Ch10.1Exercises/MyClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch10._1Exercises
+{
+    internal class MyClass
+    {
+        protected string MyString;
+        public virtual string GetString()
+        {
+            return MyString;
+        }
+        public string ContainedString
+        {
+            set
+            {
+                MyString = value;
+            }
+        }
+    }
+}
---
+++ Chapter10/Ch10Exercises/Ch10.1Exercises/MyDerivedClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch10._1Exercises
+{
+    internal class MyDerivedClass : MyClass
+    {
+        public override string GetString()
+        {
+            return MyString + "(output from derived class)";
+        }
+    }
+}
